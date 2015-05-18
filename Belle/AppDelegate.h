271cc7f61ede33dae6a399b24f47d26ad75c993a
herocule <hero@herocules-Mac.local>
2015-05-18 11:22:49 +0800

    Initial Commit

---
+++ Belle/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Belle
+//
+//  Created by herocule on 5/18/15.
+//  Copyright (c) 2015 herocule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
